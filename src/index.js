5259c2ceb4b8ad8fba5a632baa2fd6246715a8f3
Vladimir Zinovik <zinovikv@gmail.com>
2022-03-07 18:21:29 +0300

    feat: try add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let brack = {
+    ')': '(',
+    '}': '{',
+    ']': '[',
+    '|': '|'
+  };
+
+  for (let i = 0; i < str.length; i++) {
+    let currentBracket = str[i];
+
+    if (isClosedBracket(currentBracket)) {
+      if (brack[currentBracket] !== stack.pop()) return false;
+    } else {
+      stack.push(currentBracket);
+    }
+
+  }
+
+  function isClosedBracket(char) {
+    return [')', '}', ']','|'].indexOf(char) > -1;
+  }
+
+  return stack.length === 0;
 }
